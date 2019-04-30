bd22c73b1a2f105c6435f10e56d1cba4dcafb05d
Summer <summer.d.howard@gmail.com>
2019-04-30 12:31:20 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Use an API to search and display a list (API is http://cpsc-api.herokuapp.com/api)
---
+++ src/squareWithFormatting.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+export default class SquareWithFormatting extends Component {
+    render() {
+        return (
+            <div className="col-sm-4">
+                <span className="badge badge-secondary">
+                    {this.formatGene(this.props.internalState)}
+                </span>
+            </div>
+        );
+    }
+
+    formatGene(internalState) {
+        if (internalState === true) {
+            return 'True';
+        } else {
+            return 'False';
+        }
+    }
+}
